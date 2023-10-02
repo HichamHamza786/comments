992cdf70fa360c7876bff44bfc6c2b0e6254495a
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-10-02 20:26:13 +0200

    add_comment_form

---
+++ src/Form/CommentType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('content', TextareaType::class, ['label' => 'Contenu'])
+            ->add('author', TextType::class, ['label' => 'Auteur'])
+            ->add('save', SubmitType::class, ['label' => 'Soumettre']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
